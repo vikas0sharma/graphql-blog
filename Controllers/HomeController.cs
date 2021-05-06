e06135e701cd20dcdc091b909dd0e143600de74b
Vikas Sharma <mailbox.viksharma@gmail.com>
2021-05-06 21:49:57 +0530

    HomeController added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        /// <summary>Home page</summary>
+        /// <returns>View</returns>
+        [HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult HomePage()
+        {
+            return Redirect("ui/playground");
+        }
+    }
+}
